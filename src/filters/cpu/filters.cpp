78f02f943acd5afe33c12127d92050826db285be
Bugrahan <donmezbugrahan@gmail.com>
2021-01-09 02:56:34 +0300

    Refactor code

--- src/filters/cpu/filters.cpp
+++ src/filters/cpu/filters.cpp
@@ -13,23 +13,17 @@ void Filters::ApplyFilters(const cv::Mat &input_image, cv::Mat output_image) {
     cout << "Bilateral Filter:" << endl;
     cout << "\tTime for the CPU: " << (stop_s - start_s) / double(CLOCKS_PER_SEC) * 1000 << " ms" << endl;
 
-    // Blurs an image using the normalized box filter.
-    start_s = clock();
-    blur(input_image, output_image, Size(9, 9));
-    stop_s = clock();
-    cout << "Normalized Box Filter:" << endl;
-    cout << "\tTime for the CPU: " << (stop_s - start_s) / double(CLOCKS_PER_SEC) * 1000 << " ms" << endl;
-
-    // Gaussian Blur
+    // Gaussian Filter
     start_s = clock();
     GaussianBlur(input_image, output_image, Size(9, 9), 75, 75);
     stop_s = clock();
-    cout << "Gaussian Blur:" << endl;
+    cout << "Gaussian Filter:" << endl;
     cout << "\tTime for the CPU: " << (stop_s - start_s) / double(CLOCKS_PER_SEC) * 1000 << " ms" << endl;
 
+    // Median Filter
     start_s = clock();
     medianBlur(input_image, output_image, 9);
     stop_s = clock();
-    cout << "Median Blur:" << endl;
+    cout << "Median Filter:" << endl;
     cout << "\tTime for the CPU: " << (stop_s - start_s) / double(CLOCKS_PER_SEC) * 1000 << " ms" << endl;
 }
